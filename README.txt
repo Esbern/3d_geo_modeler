8a11f20e1c1d41edbf9e5aa90987a44d84df0544
Esbern Holmes <holmes@ruc.dk>
2025-01-15 13:59:53 +0100

    Licens i alle filer

---
+++ README.txt
@@ -0,0 +1,7 @@
+## License
+This plugin is licensed under the GNU General Public License v3.0 or later. See the [LICENSE](./LICENSE) file for details.
+
+## Research Participation
+This plugin was developed as part of the Innotech – TaskForce Interreg project by Esbern Holmes at Roskilde University. If you use this plugin, we kindly ask you to inform us about your usage to support follow-up research on the application of technology in urban planning. Your participation is entirely voluntary and would be highly appreciated.
+
+Contact: holmes@ruc.dk
--- processing/GeoFlow.py
+++ processing/GeoFlow.py
@@ -1,3 +1,23 @@
+# This file is part of the QGIS plugin developed for the Innotech – TaskForce Interreg project.
+# 
+# Copyright (C) [Year] Esbern Holmes, Roskilde University
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# We encourage users to inform us about their use of this plugin for research purposes.
+# Contact: holmes@ruc.dk
+
 from qgis.utils import iface
 """
 LiDARTo3DProcessingAlgorithm is a QGIS processing algorithm that converts LiDAR data into 3D building geometries using geoflow.
